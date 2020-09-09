d9872d2b6f6cf55f8d917188718601b4d1f123f1
Ben-Malik <benmaliktchamalam@gmail.com>
2020-09-09 23:55:45 +0300

    Update made on the setup

--- Solution_Ben-Malik.js
+++ Solution_Ben-Malik.js
@@ -1,11 +1,3 @@
-/**
- * Write a function in NodeJS which takes two arrays of variable length and returns a single array of the unique 
- * objects across both arrays, sorted and limited to the first X objects. Assume the object includes the 
- * following fields:
- * {Id: number, // attribute to be used to confirm uniqueness
- * rank: number // attribute to be used for sorting}
- */
-
 //A helper function to compare two given objects
 function compare(a, b) {
     if (parseInt(a.rank) < parseInt(b.rank)) {
